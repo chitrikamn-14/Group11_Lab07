61fa6d82ded23eaf162823fe015e024143c6916d
sreevanithimmigari <ee24mt022@iitdh.ac.in>
2024-10-10 23:21:18 +0530

    Updated

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
+
+uint8_t receivedByte;
+bool dataReceivedFlag = true;
+
+void UART5_send(void);
+void UART5_Transmit(uint8_t data);
